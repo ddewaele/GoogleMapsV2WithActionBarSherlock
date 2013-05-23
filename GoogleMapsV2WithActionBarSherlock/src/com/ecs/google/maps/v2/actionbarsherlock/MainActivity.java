36f89c9b6ded6b173bfb3a3f9b540f80e7efc24f
davydewaele <ddewaele@gmail.com>
2013-05-24 00:14:32 +0200

    changes

--- GoogleMapsV2WithActionBarSherlock/src/com/ecs/google/maps/v2/actionbarsherlock/MainActivity.java
+++ GoogleMapsV2WithActionBarSherlock/src/com/ecs/google/maps/v2/actionbarsherlock/MainActivity.java
@@ -1,20 +1,44 @@
 package com.ecs.google.maps.v2.actionbarsherlock;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends SherlockFragmentActivity {
     
 	public static int THEME = R.style.Theme_Sherlock;
+	private GoogleMap googleMap;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("GoogleMapsDemo");
         setContentView(R.layout.main_activity);
+        
+        
+        googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map)).getMap();
+        
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+			
+			@Override
+			public void onMapClick(LatLng latLng) {
+				final Marker marker = googleMap.addMarker(new MarkerOptions().position(latLng)
+						 .title("title")
+						 .snippet("snippet"));
+				
+    			
+			}
+
+		});
+        
     }
     
   @Override
@@ -35,4 +59,15 @@ public class MainActivity extends SherlockFragmentActivity {
 
       return true;
   }    
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+	  if (item.getItemId() == android.R.id.home || item.getItemId() == 0) {
+          return false;
+      }
+      THEME = item.getItemId();
+      
+      Toast.makeText(this, "Theme changed to \"" + item.getTitle() + "\"", Toast.LENGTH_SHORT).show();
+      return true;
+  }
 }
--- GoogleMapsV2WithActionBarSherlock/src/com/ecs/google/maps/v2/actionbarsherlock/MapFragment.java
+++ GoogleMapsV2WithActionBarSherlock/src/com/ecs/google/maps/v2/actionbarsherlock/MapFragment.java
@@ -1,20 +1,115 @@
 package com.ecs.google.maps.v2.actionbarsherlock;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.CancelableCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends SherlockMapFragment {
-	private GoogleMap mMap;
+	private GoogleMap googleMap;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View root = super.onCreateView(inflater, container, savedInstanceState);
-		mMap = getMap();
+		googleMap = getMap();
+		
+		final LatLng latLng = new LatLng(0, 0);
+		
+		final Marker marker = googleMap.addMarker(new MarkerOptions().position(latLng)
+																 .title("title")
+																 .snippet("snippet"));
+
+		final LatLng endLatLng = new LatLng(50, 4);
+		
+		final int duration = 60000;
+		
+		//navigateToPoint(latLng);
+		
+		Handler handler = new Handler();
+		final Handler mHandler = new Handler();
+		final long start = SystemClock.uptimeMillis();
+		final Interpolator interpolator = new LinearInterpolator();
+		Runnable animator = new Runnable() {
+			
+			float tilt = 0;
+			float zoom = 5.5f;
+			boolean upward=true;
+			
+			@Override
+			public void run() {
+				//System.out.println("tilt = " + tilt);
+				//System.out.println("upward = " + upward);
+				//System.out.println("zoom = " + zoom);
+				if (upward) {
+					
+					if (tilt<90) {
+						tilt ++;
+						zoom-=0.05f;
+					} else {
+						upward=false;
+					}
+					
+				} else {
+					if (tilt>0) {
+						tilt --;
+						zoom+=0.05f;
+					} else {
+						upward=true;
+					}
+				}
+				
+				long elapsed = SystemClock.uptimeMillis() - start;
+				double t = interpolator.getInterpolation((float)elapsed/duration);
+				double lat = t*endLatLng.latitude + (1-t) * latLng.latitude;
+				double lng = t*endLatLng.longitude + (1-t) * latLng.longitude;
+				LatLng latLng2 = new LatLng(lat, lng);
+				marker.setPosition(latLng2);
+				
+				//var heading = google.maps.geometry.spherical.computeHeading(point1,point2);
+				navigateToPoint(latLng2,tilt,tilt,zoom);
+				if (t< 1) {
+					mHandler.postDelayed(this, 16);
+				}
+			}
+		};
+		
+		//handler.post(animator);
+		
 		return root;
 	}
+	
+	 private void changeCamera(CameraUpdate update, CancelableCallback callback) {
+	     //googleMap.animateCamera(update, callback);
+	     googleMap.moveCamera(update);
+	 }
+
+	 private void changeCamera(CameraUpdate update) {
+		 changeCamera(update, null);
+	 }
+
+	    
+	 private void navigateToPoint(LatLng latLng,float tilt, float bearing, float zoom) {
+		 CameraPosition position =
+				 new CameraPosition.Builder().target(latLng)
+	                        .zoom(zoom)
+	                        .bearing(bearing)
+	                        .tilt(tilt)
+	                        .build();
+	    	
+	    	changeCamera(CameraUpdateFactory.newCameraPosition(position)); 
+	 }	
 }
